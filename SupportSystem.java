28ac41d09265c69aa1d3fb35fd0e72cb268aa1f6
Roberto_Sanchez <santacolombo@hotmail.com>
2014-03-24 09:15:38 +0100

    ahora el chat solo termina con bye ya sea en mayus o minus y espacios como mucho no acabaria si tiene mas caracteres que bye

--- SupportSystem.java
+++ SupportSystem.java
@@ -39,7 +39,7 @@ public class SupportSystem
         while(!finished) {
             String input = reader.getInput();
 
-            if(input.toLowerCase().trim().startsWith("bye")) {
+            if(input.toLowerCase().trim().equals("bye")) {
                 finished = true;
             }
             else {
