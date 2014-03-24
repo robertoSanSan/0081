e43184ffc0bb22b45280e4c9babd1f6f453c2fc7
Roberto_Sanchez <santacolombo@hotmail.com>
2014-03-24 09:46:11 +0100

    añadidas mas respuestas a la clase responder

--- Responder.java
+++ Responder.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -10,8 +11,17 @@ public class Responder
     /**
      * Construct a Responder - nothing to do
      */
+    Random aleatorio;
+    ArrayList<String>respuestas;
     public Responder()
     {
+        aleatorio = new Random();
+        respuestas = new ArrayList<>();
+        respuestas.add("Estas seguro?");
+        respuestas.add("necesita mas informacion?");
+        respuestas.add("cual es tu sistema operativo?");
+        respuestas.add("vale , estamos trabajando en el problema");
+        respuestas.add("puede facilitarme mas informacion?");
     }
 
     /**
@@ -20,6 +30,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int numeroObtenido = aleatorio.nextInt(respuestas.size());
+        return respuestas.get (numeroObtenido);
     }
 }
