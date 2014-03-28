6c72f094bdc0fa3b43d6ba2a5784ecb70b1b74f2
Roberto_Sanchez <santacolombo@hotmail.com>
2014-03-28 12:37:43 +0100

    ahora el programa te da respuestas coherentes a las dudas del cliente

--- Responder.java
+++ Responder.java
@@ -13,24 +13,42 @@ public class Responder
      */
     Random aleatorio;
     ArrayList<String>respuestas;
+    HashMap<String, String> respuestaCoherente;
     public Responder()
     {
         aleatorio = new Random();
         respuestas = new ArrayList<>();
+        respuestaCoherente = new HashMap<>();
         respuestas.add("Estas seguro?");
         respuestas.add("necesita mas informacion?");
         respuestas.add("cual es tu sistema operativo?");
         respuestas.add("vale , estamos trabajando en el problema");
         respuestas.add("puede facilitarme mas informacion?");
+        respuestaCoherente.put("linux" , " que software no funciona?");
+        respuestaCoherente.put("si", "que tipo de fallo le esta sucediendo?");
+        respuestaCoherente.put("vale" , "le informaremos cuando hayamos acabado");
+        respuestaCoherente.put("windows" , " que software no funciona?");
+        respuestaCoherente.put("ok", "en algo mas le podemos ayudar?");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String userInput)
     {
-        int numeroObtenido = aleatorio.nextInt(respuestas.size());
-        return respuestas.get (numeroObtenido);
+      
+        String respuesta = null;
+        respuesta = respuestaCoherente.get(userInput);
+        if (respuesta == null)
+        {
+           
+            respuesta = respuestas.get(aleatorio.nextInt(respuestas.size()));
+        }
+        
+        
+        //int numeroObtenido = aleatorio.nextInt(respuestas.size());
+        //return respuestas.get (numeroObtenido);
+        return respuesta;
     }
 }
